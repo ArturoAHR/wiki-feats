688e459b8faf8702f9693d87f511eada856bd0f0
Arturo Hernández <a.hernandez@gbh.com.do>
2024-03-26 20:40:20 -0400

    Enable Shutdown Hooks
    Prevents MikroORM from staying connected to the database after shutting
    down the app.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Prevents MikroORM from staying connected after the process is terminated.
+  app.enableShutdownHooks();
+
   await app.listen(3000);
 }
 bootstrap();
