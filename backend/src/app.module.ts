e853c6023031fca57a8245e6c3f40d06b4b60b17
Arturo Hernández <a.hernandez@gbh.com.do>
2024-03-28 13:38:29 -0400

    Remove unused App Service and Controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,10 @@
 import { MikroOrmModule } from "@mikro-orm/nestjs";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { config } from "./config/mikro-orm.config";
 import { FeedModule } from "./modules/feed/feed.module";
 
 @Module({
   imports: [ConfigModule.forRoot(), MikroOrmModule.forRoot(config), FeedModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
