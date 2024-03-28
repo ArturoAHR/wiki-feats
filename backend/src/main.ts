64e0ac1bdedac544ee068f749bef741387ceff21
Arturo Hernández <a.hernandez@gbh.com.do>
2024-03-28 13:37:28 -0400

    Set up Swagger to document endpoints

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,20 @@
 import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
+import { swaggerConfig } from "./config/swagger.config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const environment = configService.get("ENVIRONMENT");
+
+  if (environment === "development") {
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup("api", app, document);
+  }
 
   //Prevents MikroORM from staying connected after the process is terminated.
   app.enableShutdownHooks();
