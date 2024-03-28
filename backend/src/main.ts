0f3a48b1e8d19994b84da8eff9c3847e7a79030a
Arturo Hernández <a.hernandez@gbh.com.do>
2024-03-28 13:40:26 -0400

    Implement PORT environment variable in app setup
    The app will now listen on the port specified in the local .env file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
 
-  await app.listen(3000);
+  await app.listen(configService.get("PORT"));
 }
 bootstrap();
