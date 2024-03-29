0583558d6b86f008fe543706514faf54824f6394
Arturo Hernández <a.hernandez@gbh.com.do>
2024-03-29 02:03:23 -0400

    Fix feed module tests and add tests for new use cases
    Also add expect utility functions

--- backend/src/modules/feed/feed.controller.spec.ts
+++ backend/src/modules/feed/feed.controller.spec.ts
@@ -1,24 +1,29 @@
 import { createMock } from "@golevelup/ts-jest";
 import { Test, TestingModule } from "@nestjs/testing";
-import { startOfToday } from "date-fns";
-import { getIsoDate } from "../../common/helpers/iso-date.utils";
 import { createArticleMock } from "../../mocks/article.mocks";
+import { createLanguageMock } from "../../mocks/language.mocks";
 import { createPaginatedFindResultMock } from "../../mocks/pagination.mocks";
 import { FeedController } from "./feed.controller";
 import { FeedService } from "./feed.service";
 
 describe("FeedController", () => {
   let controller: FeedController;
-  const mockFeedService = createMock<FeedService>({
-    getFeedArticles: jest.fn(),
-  });
+  let mockFeedService;
 
   const articles = Array.from({ length: 12 }, () => createArticleMock());
   const paginatedArticles = createPaginatedFindResultMock(articles);
 
-  const defaultOptions = { page: undefined, pageSize: undefined };
+  const languages = Array.from({ length: 3 }, () => createLanguageMock());
 
   beforeEach(async () => {
+    mockFeedService = createMock<FeedService>({
+      getFeedArticles: jest.fn(),
+      getSupportedFeedLanguages: jest.fn(),
+    });
+
+    mockFeedService.getFeedArticles.mockResolvedValue(paginatedArticles);
+    mockFeedService.getSupportedFeedLanguages.mockResolvedValue(languages);
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FeedController],
       providers: [{ provide: FeedService, useValue: mockFeedService }],
@@ -32,40 +37,105 @@ describe("FeedController", () => {
   });
 
   it("should get today's feed articles", async () => {
-    const today = getIsoDate(startOfToday());
-    mockFeedService.getFeedArticles.mockResolvedValue(paginatedArticles);
+    const query = {};
 
-    const result = await controller.getCurrentFeed({});
+    const result = await controller.getCurrentFeed(query);
     expect(result).toEqual(paginatedArticles);
 
-    expect(mockFeedService.getFeedArticles).toHaveBeenCalledWith(
-      today,
-      defaultOptions,
-    );
+    expect(mockFeedService.getFeedArticles).toHaveBeenCalledWith({ ...query });
+  });
+
+  it("should get today's feed articles with pagination options", async () => {
+    const query = { page: 1, pageSize: 10 };
+
+    const result = await controller.getCurrentFeed(query);
+    expect(result).toEqual(paginatedArticles);
+
+    expect(mockFeedService.getFeedArticles).toHaveBeenCalledWith({ ...query });
   });
 
   it("should get feed articles by date", async () => {
-    const date = "2021-09-01";
-    mockFeedService.getFeedArticles.mockResolvedValue(paginatedArticles);
+    const params = { date: "2021-09-01" };
+    const query = {};
 
-    const result = await controller.getFeedByDate({ date }, {});
+    const result = await controller.getFeedByDate(params, query);
     expect(result).toEqual(paginatedArticles);
 
-    expect(mockFeedService.getFeedArticles).toHaveBeenCalledWith(
-      date,
-      defaultOptions,
-    );
+    expect(mockFeedService.getFeedArticles).toHaveBeenCalledWith({
+      ...params,
+      ...query,
+    });
   });
 
   it("should get feed articles by date with pagination options", async () => {
-    const date = "2021-09-01";
-    const options = { page: 1, pageSize: 10 };
+    const params = { date: "2021-09-01" };
+    const query = { page: 1, pageSize: 10 };
 
-    mockFeedService.getFeedArticles.mockResolvedValue(paginatedArticles);
-
-    const result = await controller.getFeedByDate({ date }, options);
+    const result = await controller.getFeedByDate(params, query);
     expect(result).toEqual(paginatedArticles);
 
-    expect(mockFeedService.getFeedArticles).toHaveBeenCalledWith(date, options);
+    expect(mockFeedService.getFeedArticles).toHaveBeenCalledWith({
+      ...params,
+      ...query,
+    });
+  });
+
+  it("should get today's feed articles in the given language", async () => {
+    const params = { languageCode: "es" };
+    const query = {};
+
+    const result = await controller.getTranslatedFeed(params, query);
+    expect(result).toEqual(paginatedArticles);
+
+    expect(mockFeedService.getFeedArticles).toHaveBeenCalledWith({
+      ...params,
+      ...query,
+    });
+  });
+
+  it("should get today's feed articles with pagination options in the given language", async () => {
+    const params = { languageCode: "es" };
+    const query = { page: 1, pageSize: 10 };
+
+    const result = await controller.getTranslatedFeed(params, query);
+    expect(result).toEqual(paginatedArticles);
+
+    expect(mockFeedService.getFeedArticles).toHaveBeenCalledWith({
+      ...params,
+      ...query,
+    });
+  });
+
+  it("should get feed articles by date", async () => {
+    const params = { languageCode: "es", date: "2021-09-01" };
+    const query = {};
+
+    const result = await controller.getTranslatedFeedByDate(params, query);
+    expect(result).toEqual(paginatedArticles);
+
+    expect(mockFeedService.getFeedArticles).toHaveBeenCalledWith({
+      ...params,
+      ...query,
+    });
+  });
+
+  it("should get feed articles by date with pagination options", async () => {
+    const params = { languageCode: "es", date: "2021-09-01" };
+    const query = { page: 1, pageSize: 10 };
+
+    const result = await controller.getTranslatedFeedByDate(params, query);
+    expect(result).toEqual(paginatedArticles);
+
+    expect(mockFeedService.getFeedArticles).toHaveBeenCalledWith({
+      ...params,
+      ...query,
+    });
+  });
+
+  it("should get supported feed languages", async () => {
+    const result = await controller.getSupportedFeedLanguages();
+    expect(result).toEqual(languages);
+
+    expect(mockFeedService.getSupportedFeedLanguages).toHaveBeenCalled();
   });
 });
--- backend/src/modules/feed/feed.service.spec.ts
+++ backend/src/modules/feed/feed.service.spec.ts
@@ -1,26 +1,66 @@
 import { createMock } from "@golevelup/ts-jest";
 import { Test, TestingModule } from "@nestjs/testing";
+import {
+  expectCallsInMethods,
+  expectNoCallsInMethods,
+} from "../../common/helpers/expect.utils";
 import { createArticleMock } from "../../mocks/article.mocks";
+import { createLanguageMock } from "../../mocks/language.mocks";
 import { createPaginatedFindResultMock } from "../../mocks/pagination.mocks";
 import { ArticleService } from "../article/article.service";
+import { TranslateService } from "../translate/translate.service";
 import { FeedService } from "./feed.service";
 
 describe("FeedService", () => {
   let service: FeedService;
-  const mockArticleService = createMock<ArticleService>({
-    getArticleCollectionStatus: jest.fn(),
-    getArticlesByDate: jest.fn(),
-    importArticles: jest.fn(),
-  });
+  let mockArticleService;
+  let mockTranslateService;
 
   const articles = Array.from({ length: 12 }, () => createArticleMock());
   const paginatedArticles = createPaginatedFindResultMock(articles);
 
+  const setResolvedValues = ({
+    isTranslated,
+    isImported,
+    isLanguageSupported,
+    translatedArticlesSearchResult,
+    importedArticlesSearchResult,
+  }: Partial<{
+    [key: string]: any;
+  }>) => {
+    mockTranslateService.checkIfTranslationLanguageIsSupported.mockResolvedValue(
+      isLanguageSupported,
+    );
+    mockArticleService.checkIfArticlesAreImported.mockResolvedValue(isImported);
+    mockArticleService.checkIfArticlesAreTranslated.mockResolvedValue(
+      isTranslated,
+    );
+    mockArticleService.getImportedArticlesByDate.mockResolvedValue(
+      importedArticlesSearchResult,
+    );
+    mockArticleService.getTranslatedArticlesByDate.mockResolvedValue(
+      translatedArticlesSearchResult,
+    );
+  };
+
   beforeEach(async () => {
+    mockArticleService = createMock<ArticleService>({
+      checkIfArticlesAreImported: jest.fn(),
+      translateArticles: jest.fn(),
+      getImportedArticlesByDate: jest.fn(),
+      getTranslatedArticlesByDate: jest.fn(),
+      importArticles: jest.fn(),
+    });
+
+    mockTranslateService = createMock({
+      checkIfTranslationLanguageIsSupported: jest.fn(),
+    });
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         FeedService,
         { provide: ArticleService, useValue: mockArticleService },
+        { provide: TranslateService, useValue: mockTranslateService },
       ],
     }).compile();
 
@@ -32,27 +72,131 @@ describe("FeedService", () => {
   });
 
   it("should get feed articles if they are already imported", async () => {
-    mockArticleService.getArticleCollectionStatus.mockResolvedValue(true);
-    mockArticleService.getArticlesByDate.mockResolvedValue(paginatedArticles);
+    setResolvedValues({
+      isTranslated: true,
+      isImported: true,
+      isLanguageSupported: true,
+      importedArticlesSearchResult: paginatedArticles,
+    });
 
-    const result = await service.getFeedArticles("2021-09-01");
+    const result = await service.getFeedArticles({});
 
     expect(result).toEqual(paginatedArticles);
 
-    expect(mockArticleService.getArticleCollectionStatus).toHaveBeenCalled();
-    expect(mockArticleService.getArticlesByDate).toHaveBeenCalled();
+    expectCallsInMethods([
+      mockTranslateService.checkIfTranslationLanguageIsSupported,
+      mockArticleService.checkIfArticlesAreImported,
+      mockArticleService.checkIfArticlesAreTranslated,
+      mockArticleService.getImportedArticlesByDate,
+    ]);
+
+    expectNoCallsInMethods([
+      mockArticleService.importArticles,
+      mockArticleService.translateArticles,
+      mockArticleService.getTranslatedArticlesByDate,
+    ]);
   });
 
   it("should import articles if they are not imported", async () => {
-    mockArticleService.getArticleCollectionStatus.mockResolvedValue(false);
-    mockArticleService.getArticlesByDate.mockResolvedValue(paginatedArticles);
+    setResolvedValues({
+      isTranslated: true,
+      isImported: false,
+      isLanguageSupported: true,
+      importedArticlesSearchResult: paginatedArticles,
+    });
 
-    const result = await service.getFeedArticles("2021-09-01");
+    const result = await service.getFeedArticles({});
 
     expect(result).toEqual(paginatedArticles);
 
-    expect(mockArticleService.getArticleCollectionStatus).toHaveBeenCalled();
-    expect(mockArticleService.importArticles).toHaveBeenCalled();
-    expect(mockArticleService.getArticlesByDate).toHaveBeenCalled();
+    expectCallsInMethods([
+      mockTranslateService.checkIfTranslationLanguageIsSupported,
+      mockArticleService.checkIfArticlesAreImported,
+      mockArticleService.checkIfArticlesAreTranslated,
+      mockArticleService.importArticles,
+      mockArticleService.getImportedArticlesByDate,
+    ]);
+
+    expectNoCallsInMethods([
+      mockArticleService.translateArticles,
+      mockArticleService.getTranslatedArticlesByDate,
+    ]);
+  });
+
+  it("should translate articles if they are not translated", async () => {
+    setResolvedValues({
+      isTranslated: false,
+      isImported: true,
+      isLanguageSupported: true,
+      translatedArticlesSearchResult: paginatedArticles,
+    });
+
+    const result = await service.getFeedArticles({ languageCode: "es" });
+
+    expect(result).toEqual(paginatedArticles);
+
+    expectCallsInMethods([
+      mockTranslateService.checkIfTranslationLanguageIsSupported,
+      mockArticleService.checkIfArticlesAreImported,
+      mockArticleService.checkIfArticlesAreTranslated,
+      mockArticleService.translateArticles,
+      mockArticleService.getTranslatedArticlesByDate,
+    ]);
+
+    expectNoCallsInMethods([
+      mockArticleService.importArticles,
+      mockArticleService.getImportedArticlesByDate,
+    ]);
+  });
+
+  it("should import and translate articles if they are neither translated nor imported ", async () => {
+    setResolvedValues({
+      isTranslated: false,
+      isImported: false,
+      isLanguageSupported: true,
+      translatedArticlesSearchResult: paginatedArticles,
+    });
+
+    const result = await service.getFeedArticles({ languageCode: "es" });
+
+    expect(result).toEqual(paginatedArticles);
+
+    expectCallsInMethods([
+      mockTranslateService.checkIfTranslationLanguageIsSupported,
+      mockArticleService.checkIfArticlesAreImported,
+      mockArticleService.checkIfArticlesAreTranslated,
+      mockArticleService.importArticles,
+      mockArticleService.translateArticles,
+      mockArticleService.getTranslatedArticlesByDate,
+    ]);
+
+    expectNoCallsInMethods([mockArticleService.getImportedArticlesByDate]);
+  });
+
+  it("should throw an error if the language is not supported", async () => {
+    setResolvedValues({
+      isTranslated: false,
+      isImported: false,
+      isLanguageSupported: false,
+    });
+
+    await expect(
+      service.getFeedArticles({ languageCode: "es" }),
+    ).rejects.toThrow("The requested language is not supported");
+
+    expectCallsInMethods([
+      mockTranslateService.checkIfTranslationLanguageIsSupported,
+    ]);
+  });
+
+  it("should get supported feed languages", async () => {
+    const languages = Array.from({ length: 5 }, () => createLanguageMock());
+    mockTranslateService.getSupportedLanguages.mockResolvedValue(languages);
+
+    const result = await service.getSupportedFeedLanguages();
+
+    expect(result).toEqual(languages);
+
+    expectCallsInMethods([mockTranslateService.getSupportedLanguages]);
   });
 });
