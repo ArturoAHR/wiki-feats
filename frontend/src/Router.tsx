c3318c079a12ff2b983b68280003a5c852974449
Arturo Hernández <a.hernandez@gbh.com.do>
2024-03-29 15:32:04 -0400

    Add home page that redirect to feed page

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/home/Home";
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<div></div>} />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 };
