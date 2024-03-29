f8992e4e15088362269214a74d7a3b19302ab50b
Arturo Hernández <a.hernandez@gbh.com.do>
2024-03-29 16:10:35 -0400

    Add feed page

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import { Feed } from "./pages/feed/Feed";
 import { Home } from "./pages/home/Home";
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/feed" element={<Feed />} />
     </Routes>
   );
 };
---
+++ frontend/src/pages/feed/Feed.tsx
@@ -0,0 +1,8 @@
+export const Feed = () => {
+  return (
+    <div className="feed-page-content">
+      <div className="feed-header">Featured Article Feed</div>
+      <div className="feed-articles">Render articles here</div>
+    </div>
+  );
+};
