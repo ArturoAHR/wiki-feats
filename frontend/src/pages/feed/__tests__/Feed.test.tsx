f7d54b115a984178a73638a7f172be4a5681e4c6
Arturo Hernández <a.hernandez@gbh.com.do>
2024-03-30 00:55:23 -0400

    Fix feed page tests

--- frontend/src/pages/feed/__tests__/Feed.test.tsx
+++ frontend/src/pages/feed/__tests__/Feed.test.tsx
@@ -5,6 +5,15 @@ vi.mock("../../../components/article-feed/ArticleFeed.tsx", () => ({
   ArticleFeed: () => <div data-testid="article-feed" />,
 }));
 
+vi.mock(
+  "../../../components/article-feed-params-fields/ArticleFeedParamsFields.tsx",
+  () => ({
+    ArticleFeedParamsFields: () => (
+      <div data-testid="article-feed-params-fields" />
+    ),
+  }),
+);
+
 describe("Feed Page", () => {
   test("should render correctly", () => {
     render(<Feed />);
